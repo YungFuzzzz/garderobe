834e4b44b944094ce673e804aeacb3379c1a405c
FUZZZ <147380081+YungFuzzzz@users.noreply.github.com>
2024-12-08 00:36:33 +0100

    fix: trying again

--- classes/Db.php
+++ classes/Db.php
@@ -14,18 +14,15 @@ class Db
     {
         if (self::$conn === null) {
             try {
-                // Laad de .env bestand vanuit de root directory
-                $dotenv = Dotenv::createImmutable(__DIR__ . '/../../');  // Dit gaat twee niveaus omhoog naar de root van je project.
+                $dotenv = Dotenv::createImmutable(__DIR__ . '/../');
                 $dotenv->load();
 
-                // Verkrijg de database instellingen uit de .env
                 $host = $_ENV['DB_HOST'];
                 $port = $_ENV['DB_PORT'];
                 $dbname = $_ENV['DB_NAME'];
                 $user = $_ENV['DB_USER'];
                 $password = $_ENV['DB_PASSWORD'];
 
-                // Maak de verbinding
                 self::$conn = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $user, $password);
                 self::$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             } catch (PDOException $e) {
