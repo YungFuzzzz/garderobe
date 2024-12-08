3c479dd55aee0ab6c7e1b70a0b6b571d96b9a24a
FUZZZ <147380081+YungFuzzzz@users.noreply.github.com>
2024-12-08 01:18:25 +0100

    fix: tried railway variables

--- classes/Db.php
+++ classes/Db.php
@@ -5,8 +5,6 @@ namespace Faisalcollinet\Wardrobe;
 use PDO;
 use PDOException;
 
-require_once __DIR__ . '/../loadEnv.php';  // Dit laad de loadEnv functie vanuit de root van je project
-
 class Db
 {
     private static $conn = null;
@@ -15,21 +13,12 @@ class Db
     {
         if (self::$conn === null) {
             try {
-                // Bepaal het pad naar het .env bestand op basis van de omgeving
-                $envPath = '/app/.env';  // Standaard pad voor Railway omgeving
-                if (isset($_ENV['RAILWAY_ENVIRONMENT_NAME']) && $_ENV['RAILWAY_ENVIRONMENT_NAME'] === 'production') {
-                    $envPath = '/workspace/.env';  // Probeer dit pad voor productieomgeving op Railway
-                }
-
-                // Laad het .env bestand
-                loadEnv($envPath);
-
-                // Verkrijg de database instellingen uit de $_ENV variabelen
-                $host = $_ENV['DB_HOST'];
-                $port = $_ENV['DB_PORT'];
-                $dbname = $_ENV['DB_NAME'];
-                $user = $_ENV['DB_USER'];
-                $password = $_ENV['DB_PASSWORD'];
+                // Verkrijg de database instellingen uit de omgevingsvariabelen
+                $host = getenv('DB_HOST');
+                $port = getenv('DB_PORT');
+                $dbname = getenv('DB_NAME');
+                $user = getenv('DB_USER');
+                $password = getenv('DB_PASSWORD');
 
                 // Maak de verbinding
                 self::$conn = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $user, $password);
